a37b1ff01262a5a0c46ff88f2a8f400b862adecd
Adithya-CSM <adithyasaireddy99@gmail.com>
2026-03-01 22:29:25 +0530

    code fix

--- frontend-new/src/main.jsx
+++ frontend-new/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext'
+import './i18n'
 
 import './index.css'
 import './App.css'
@@ -15,6 +16,5 @@ createRoot(document.getElementById('root')).render(
             </StrictMode>
         </UserContext>
     </BrowserRouter>
-
 )
 
